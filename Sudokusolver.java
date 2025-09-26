d5e47746be8493d653dcfeae58c6725c72cb3394
AlexDanglli <168667746+AlexDanglli@users.noreply.github.com>
2025-09-26 14:39:33 -0600

    Update Sudokusolver.java

--- Sudokusolver.java
+++ Sudokusolver.java
@@ -5,9 +5,23 @@ import java.io.FileNotFoundException;
 public class Sudokusolver {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		Board puzzle = new Board();
-		puzzle.loadPuzzle("easy.txt");
+
+    	 Cell potentialCheck = new Cell();
+        boolean[] potentials = {false, true, false, true, false, false, false, false, false, false};
+        potentialCheck.setPotential(potentials);
+
+        int firstPotential = potentialCheck.getFirstPotential();
+        int secondPotential = potentialCheck.getSecondPotential();
+
+        if(firstPotential != 1 || secondPotential != 3)
+        {
+            throw new AssertionError("Unexpected potential positions: first=" + firstPotential + ", second=" + secondPotential);
+        }
+
+        System.out.println("Cell potential check passed: first=" + firstPotential + ", second=" + secondPotential);
+
+        Board puzzle = new Board();
+        puzzle.loadPuzzle("easy.txt");
 		puzzle.display();
 		puzzle.logicCycles();
 		puzzle.display();
